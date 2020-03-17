09cfd69a84d72b7a03e08ddfc2b9d8c1ae1af324
txya900619 <wayne900619@gmail.com>
2020-03-18 01:13:00 +0800

    add flask router methods

--- server.py
+++ server.py
@@ -5,32 +5,32 @@ app = Flask(__name__, static_folder='website-new', static_url_path='')
 app.config['JSON_AS_ASCII'] = False
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def give_html():
     return app.send_static_file('index.html')
 
 
-@app.route('/<path:path>')
+@app.route('/<path:path>', methods=["GET"])
 def give_js(path):
     return send_from_directory('website-new', path)
 
 
-@app.route('/api/article/<article_id>')
+@app.route('/api/article/<article_id>', methods=["GET"])
 def article_info(article_id):
     return cra.get_article(article_id)
 
 
-@app.route('/api/article/<article_id>/content')
+@app.route('/api/article/<article_id>/content', methods=["GET"])
 def article_content(article_id):
     return cra.get_article(article_id, True)
 
 
-@app.route('/api/article_list/<int:page>')
+@app.route('/api/article_list/<int:page>', methods=["GET"])
 def article_ids(page):
     return cra.get_article_ids(page)
 
 
-@app.route('/api/article_list/')
+@app.route('/api/article_list/', methods=["GET"])
 def latest_page_article_ids():
     return cra.get_article_ids()
 
