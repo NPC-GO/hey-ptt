5f35e13a1ed0491a94fb4e8fbcb6180931028af8
Xanonymous <trusaidlin@gmail.com>
2020-03-18 03:57:14 +0800

    add page navigation buttons and their javascript.

--- website-new/js/index.js
+++ website-new/js/index.js
@@ -1,3 +1,20 @@
+let prevPage, lastPage, latestPage;
+(async function () {
+  let prevButton = document.getElementById("prev-button");
+  prevButton.addEventListener("click", async () => {
+    await showList(prevPage || "");
+  });
+  let nextButton = document.getElementById("next-button");
+  nextButton.addEventListener("click", async () => {
+    await showList(lastPage || "");
+  });
+  let latestButton = document.getElementById("latest-button");
+  latestButton.addEventListener("click", async () => {
+    await showList("");
+  });
+  await showList("");
+}());
+
 function request(url, method, ...header) {
   return new Promise(function (resolve, reject) {
     let httpRequest = new XMLHttpRequest();
@@ -30,8 +47,20 @@ async function getList(page) {
 }
 
 async function showList(page) {
-  let [list, prevPage] = await getList(page);
+  let prevButton = document.getElementById("prev-button");
+  let nextButton = document.getElementById("next-button");
+  let latestButton = document.getElementById("latest-button");
+  [prevButton, nextButton, latestButton].forEach((button) => {
+    button.disabled = true;
+    button.classList.add("disabled");
+  });
+  let list;
+  [list, prevPage] = await getList(page);
+  if (!latestPage) {
+    latestPage = String(Number(prevPage) + 1);
+  }
   let listContainer = document.getElementById("list");
+  listContainer.innerHTML = "";
   (await Promise.all(list.map(async (articleId) => {
     let cardData = await getArticleTitle(articleId);
     return {
@@ -61,13 +90,20 @@ async function showList(page) {
     card.appendChild(cardInfo);
     listContainer.appendChild(card);
   });
+  latestButton.disabled = false;
+  latestButton.classList.remove("disabled");
+  lastPage = String(Number(prevPage) + 2);
+  if (prevPage) {
+    prevButton.disabled = false;
+    prevButton.classList.remove("disabled");
+  }
+  if (lastPage && Number(lastPage) <= Number(latestPage)) {
+    nextButton.disabled = false;
+    nextButton.classList.remove("disabled");
+  }
 }
 
 async function showArticle(articleId) {
   let article = await getArticleWithContent(articleId);
   //show
 }
-
-(function () {
-  showList("");
-}());
