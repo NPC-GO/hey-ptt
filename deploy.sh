c731130e6032b96a0a901ef6ccb5e11a28cda7f2
Xanonymous <trusaidlin@gmail.com>
2020-03-23 00:21:15 +0800

    sudo kill u hahahahaah

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,7 @@ git clone https://github.com/NPC-GO/hey-ptt.git
 cd hey-ptt
 pip3 install -r requirements.txt
 export FLASK_APP=app.py
-if [ -s ./certs ]
-  then
-    echo Hi
-  else
-    mkdir certs
-  fi
+sudo rm -rf ./certs
+mkdir certs
 mv ../certs/* ./certs
 screen sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443 && exit
