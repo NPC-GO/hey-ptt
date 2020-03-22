7aa4e3408c9e9512571aca39d8848c3804b26417
Xanonymous <trusaidlin@gmail.com>
2020-03-22 22:32:01 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ rm -rf ./hey-ptt
 git clone https://github.com/NPC-GO/hey-ptt.git
 # shellcheck disable=SC2164
 cd hey-ptt
-pip freeze > requirements.txt
+pip3 freeze > requirements.txt
 pip3 install -r requirements.txt
 export FLASK_APP=app.py
 sudo flask run --cert ../certs/server.pub --key ../certs/server.key --host=0.0.0.0 --port=443
