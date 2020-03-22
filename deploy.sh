588557f6ef6341d18adab3fa5772f66e78d1a02f
Xanonymous <trusaidlin@gmail.com>
2020-03-22 22:09:10 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd hey-ptt
 pip freeze > requirements.txt
 pip3 install -r requirements.txt
 export FLASK_APP=app.py
-sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443
+sudo flask run --cert ../certs/server.pub --key ../certs/server.key --host=0.0.0.0 --port=443
