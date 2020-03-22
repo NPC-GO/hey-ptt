6f6ba3541b0ed799a6cd441d03d146e7001f9261
Xanonymous <trusaidlin@gmail.com>
2020-03-23 01:17:15 +0800

    beautiful

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ git clone https://github.com/NPC-GO/hey-ptt.git
 cd hey-ptt
 pip3 install -r requirements.txt
 export FLASK_APP=app.py
-sudo rm -rf ./certs
 mkdir certs
 mv ../certs/* ./certs
-screen sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443 && exit
+screen sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443
