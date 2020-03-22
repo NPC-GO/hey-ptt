813561f2baf0152ce18346cdc04026b9be9cd080
Xanonymous <trusaidlin@gmail.com>
2020-03-23 01:39:26 +0800

    ?

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ pip3 install -r requirements.txt
 export FLASK_APP=app.py
 mkdir certs
 mv ../certs/* ./certs
-screen sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443
+screen -d -m sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443
