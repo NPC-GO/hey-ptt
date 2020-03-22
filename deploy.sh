c15bad33ed1c0fe01e692930e34a231510b83ccd
Xanonymous <trusaidlin@gmail.com>
2020-03-22 22:57:02 +0800

    change cert position

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ cd hey-ptt
 pip3 freeze > requirements.txt
 pip3 install -r requirements.txt
 export FLASK_APP=app.py
+mkdir certs
+mv ../certs/* ./certs
 sudo flask run --cert ./certs/server.pub --key ./certs/server.key --host=0.0.0.0 --port=443
