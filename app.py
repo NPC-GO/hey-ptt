c874acf9f76fbac8a2e2e493baa88894bd20259d
TeU-Lin <trusaidlin@gmail.com>
2020-03-30 03:31:57 +0800

    Support img yt comments (#8)
    * add html renderer to find the .jpg ...etc text and change to <img> tag.
    
    * fix img position, force img url using https
    
    * change renderer to be imported as a module.
    
    * remove unneeded comment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import crawler as cra
+from renderer import render
 
 app = Flask(__name__, static_folder='website-new', static_url_path='')
 app.config['JSON_AS_ASCII'] = False
@@ -19,7 +20,8 @@ def article_info(article_id):
 @app.route('/api/articles/<article_id>', methods=['GET'])
 def article_content(article_id):
     board = request.args.get('board')
-    return cra.get_article(article_id, board, is_include_content=True)
+    raw_data = cra.get_article(article_id, board, is_include_content=True)
+    return render(raw_data)
 
 
 @app.route('/api/articles', methods=['GET'])
---
+++ renderer.py
@@ -0,0 +1,24 @@
+import re
+
+
+def render(data: dict) -> dict:
+    r = render_img_content(data)
+    r = render_https_content(r)
+    return r
+
+
+def render_img_content(data: dict) -> dict:
+    content = data["content"]
+    img_links = re.findall(r"(?:http:|https:)?//.*\.(?:png|jpg|gif)", content)
+    for img_link in img_links:
+        pos = content.find(img_link)
+        new = "<img alt src='{}'>".format(img_link)
+        content = content[:pos] + new + content[pos + len(img_link):]
+    content = "<br />".join(content.split("\n"))
+    data["content"] = content
+    return data
+
+
+def render_https_content(data: dict) -> dict:
+    data["content"].replace("http", "https")
+    return data
