469c928c637d9bf6c7e15d677e19e490b32157de
Xanonymous <trusaidlin@gmail.com>
2020-04-05 22:10:04 +0800

    hide protected email a tag and text

--- renderer.py
+++ renderer.py
@@ -1,10 +1,11 @@
 import re
-# from bs4 import BeautifulSoup as b
+from bs4 import BeautifulSoup as b
 
 
 def render(data: dict) -> dict:
     # r = render_img_content(data)
-    r = render_https_content(data)
+    r = email_protected(data)
+    r = render_https_content(r)
     return r
 
 
@@ -17,6 +18,17 @@ def render(data: dict) -> dict:
 #         print(x.get("data-id"))
 #     data["content"] = content
 #     return data
+
+def email_protected(data: dict) -> dict:
+    if "__cf_email__" in data["content"]:
+        content = data["content"]
+        soup = b(content, 'html.parser')
+        protected_emails = soup.find_all("a", class_="__cf_email__")
+        for protected_email in protected_emails:
+            protected_email.name = "span"
+            protected_email.string = ""
+        data["content"] = str(soup)
+    return data
 
 
 def render_https_content(data: dict) -> dict:
