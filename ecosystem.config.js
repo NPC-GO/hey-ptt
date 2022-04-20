a51e86aeb6e2a75d1497b5949590a02b0e17f9c5
Xanonymous <trusaidlin@gmail.com>
2022-04-21 07:29:19 +0800

    chore: remove pm2 config files

--- ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps: [
-        {
-            name: 'HEY-PTT',
-            script: 'sudo flask run --cert ./certs/server.pub --key ./certs/server.key --port=443 --host=0.0.0.0'
-        }
-    ]
-}
